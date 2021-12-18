05b69fec36ec2ed0d58e03886fd8ee90d8a287e4
aiae <bgdppeu@gmail.com>
2021-12-18 21:09:10 +0200

    Prizes page

--- resources/views/prizes/prizes.blade.php
+++ resources/views/prizes/prizes.blade.php
@@ -1,7 +1,44 @@
 @extends('parts.base')
 
 @section('content')
-    <div class="container mt-3">
-
+    <div class="container mt-3" id="prizes">
+        <div class="row justify-content-center">
+            <div class="col-lg-3">
+                <div class="card third-place">
+                    <div class="">
+                        <div class="medal">🥉</div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">$50 USD</li>
+                            <li class="list-group-item">1 Months Donator</li>
+                            <li class="list-group-item">Profile Badge</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card first-place">
+                    <div class="">
+                        <div class="medal">🥇</div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">$150 USD</li>
+                            <li class="list-group-item">3 Months Donator</li>
+                            <li class="list-group-item">Profile Badge</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+            <div class="col-lg-3">
+                <div class="card second-place">
+                    <div class="">
+                        <div class="medal">🥈</div>
+                        <ul class="list-group list-group-flush">
+                            <li class="list-group-item">$100 USD</li>
+                            <li class="list-group-item">2 Months Donator</li>
+                            <li class="list-group-item">Profile Badge</li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </div>
     </div>
 @endsection
