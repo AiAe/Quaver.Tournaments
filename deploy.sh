e1f71104665b7b76981603365ec47f6672df2ee1
AiAe <ayyayyedev@gmail.com>
2021-12-20 15:01:54 +0200

    server changes

---
+++ deploy.sh
@@ -0,0 +1,17 @@
+/usr/bin/git pull
+
+/usr/sbin/composer install --no-dev
+
+/usr/bin/php artisan migrate --force
+
+/usr/bin/php artisan cache:clear
+/usr/bin/php artisan config:clear
+/usr/bin/php artisan view:clear
+/usr/bin/php artisan route:clear
+
+/usr/bin/php artisan optimize:clear
+/usr/bin/php artisan package:discover
+/usr/bin/php artisan view:cache
+/usr/bin/php artisan route:cache
+/usr/bin/php artisan config:cache
+/usr/bin/php artisan storage:link
