c44b81cb0dbdf749a554137c5720a5f732878c8b
aiae <bgdppeu@gmail.com>
2021-12-21 15:06:06 +0200

    Fix links

--- resources/views/parts/header.blade.php
+++ resources/views/parts/header.blade.php
@@ -1,6 +1,6 @@
 <nav class="navbar navbar-expand-lg navbar-dark bg-qot">
     <div class="container">
-        <a class="navbar-brand" href="#">{{ __('Quaver Official Tournament') }}</a>
+        <a class="navbar-brand" href="{{ route('home') }}">{{ __('Quaver Official Tournament') }}</a>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar"
                 aria-controls="navbar" aria-expanded="false" aria-label="{{ __('Toggle navigation') }}">
             <span class="navbar-toggler-icon"></span>
