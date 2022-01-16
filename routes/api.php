c970ff7801430126c711fba9701977b37010374e
aiae <bgdppeu@gmail.com>
2022-01-16 12:40:07 +0200

    fix api route as

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::middleware('json.response')->prefix('v1')->as('v1')->group(function() {
+Route::middleware('json.response')->prefix('v1')->as('v1.')->group(function() {
     Route::get('players', [\App\Http\Controllers\API\v1\Players\PlayersController::class, 'players']);
     Route::get('mappool', [\App\Http\Controllers\API\v1\Mappool\MappoolController::class, 'mappool']);
     Route::get('staff', [\App\Http\Controllers\API\v1\Staff\StaffController::class, 'staff']);
