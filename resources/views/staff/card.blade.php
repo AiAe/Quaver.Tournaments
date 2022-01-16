55cfe59b14fda6a83164e8f673ac577dc0a94a64
aiae <bgdppeu@gmail.com>
2022-01-16 11:38:47 +0200

    Fix long names

--- resources/views/staff/card.blade.php
+++ resources/views/staff/card.blade.php
@@ -2,9 +2,9 @@
     <a href="{{ route('quaverUser', $player['id']) }}" target="_blank">
         <img src="{{ $player['avatar_url'] }}" class="img-fluid card-img-top" alt="{{ $player['username'] }}">
     </a>
-    <div class="card-footer">
+    <div class="card-footer" style="padding: 0.5rem 0.5rem">
         <a href="{{ route('quaverUser', $player['id']) }}" target="_blank">
-            {{ Str::limit($player['username'], 12) }}
+            {{ $player['username'] }}
         </a>
     </div>
 </div>
