9d9f2e1ad3b3710e9857b2adcd742f0ec8de5b65
aiae <bgdppeu@gmail.com>
2022-02-12 22:45:49 +0200

    Enable axios

--- resources/js/web.js
+++ resources/js/web.js
@@ -4,9 +4,9 @@
  * CSRF token as a header based on the value of the "XSRF" token cookie.
  */
 
-// window.axios = require('axios');
+window.axios = require('axios');
 
-// window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
+window.axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
 
 window.bootstrap = require('./bootstrap.bundle.min');
 
