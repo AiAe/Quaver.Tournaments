86319e986d475bff874b289b04821ef02f461df8
IceDynamix <me@icedynamix.moe>
2023-01-10 23:21:41 +0100

    Create users migration

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -15,10 +15,10 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
-            $table->string('password');
+            $table->string('quaver_user_id')->unique();
+            $table->string('discord_user_id')->nullable()->index();
+            $table->string('username');
+            $table->string('country');
             $table->rememberToken();
             $table->timestamps();
         });
