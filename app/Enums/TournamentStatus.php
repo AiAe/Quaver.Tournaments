f1ec2d4b61879fbc611ea111dc25a7eede1d062a
IceDynamix <me@icedynamix.moe>
2023-01-23 21:35:03 +0100

    Increment tournament status values by 1

--- app/Enums/TournamentStatus.php
+++ app/Enums/TournamentStatus.php
@@ -4,9 +4,9 @@ namespace App\Enums;
 
 enum TournamentStatus: int
 {
-    case Unlisted = -1;
-    case Upcoming = 0;
-    case RegistrationsOpen = 1;
-    case Ongoing = 2;
-    case Concluded = 3;
+    case Unlisted = 0;
+    case Upcoming = 1;
+    case RegistrationsOpen = 2;
+    case Ongoing = 3;
+    case Concluded = 4;
 }
