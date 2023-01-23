e2ef829b1a82a0e08fbbc445dd9b63c2f8f2761b
IceDynamix <me@icedynamix.moe>
2023-01-23 21:32:38 +0100

    Move seeders into separate files

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,11 +3,6 @@
 namespace Database\Seeders;
 
 // use Illuminate\Database\Console\Seeds\WithoutModelEvents;
-use App\Enums\TournamentFormat;
-use App\Enums\TournamentStatus;
-use App\Enums\UserRoles;
-use App\Models\Tournament;
-use App\Models\User;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -19,42 +14,9 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        $this->seedUsers();
-        $this->seedTournaments();
-    }
-
-    /**
-     * @return void
-     */
-    private function seedUsers(): void
-    {
-        $users = User::factory(25)->create();
-
-        $i = 0;
-        foreach (UserRoles::cases() as $case) {
-            if ($case->value == UserRoles::User->value) {
-                continue;
-            }
-
-            $user = $users[$i];
-            $user->roles()->create(['role' => $case->value]);
-            $i++;
-        }
-    }
-
-    /**
-     * @return void
-     */
-    private function seedTournaments(): void
-    {
-        $statuses = TournamentStatus::cases();
-        Tournament::factory(5)
-            ->sequence(fn($seq) => ['status' => $statuses[$seq->index]->value])
-            ->create(['format' => TournamentFormat::Solo]);
-
-        Tournament::factory()->create([
-            'format' => TournamentFormat::Team,
-            'status' => TournamentStatus::RegistrationsOpen
+        $this->call([
+            UserSeeder::class,
+            TournamentSeeder::class
         ]);
     }
 }
