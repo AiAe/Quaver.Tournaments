504e52f0618b41015f2341021cff42ee79d23dd7
AiAe <ayyayyedev@gmail.com>
2023-01-27 10:34:18 +0200

    UserRole to name

--- app/Enums/UserRoles.php
+++ app/Enums/UserRoles.php
@@ -2,10 +2,25 @@
 
 namespace App\Enums;
 
+use App\Traits\EnumToArray;
+
 enum UserRoles : int
 {
+    use EnumToArray;
+
     case Blacklisted = 0;
     case User = 1;
     case Admin = 2;
     case Organizer = 3;
+
+    public function name()
+    {
+        return match ($this)
+        {
+            self::Blacklisted => 'Blacklisted',
+            self::User => 'User',
+            self::Admin => 'Admin',
+            self::Organizer => 'Organizer'
+        };
+    }
 }
