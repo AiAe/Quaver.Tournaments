560db588e45a89589093fb18f794359f45afc741
IceDynamix <me@icedynamix.moe>
2023-01-27 03:39:17 +0100

    Add teams resource controller skeleton

---
+++ app/Http/Controllers/Web/TeamsController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace App\Http\Controllers\Web;
+
+use App\Http\Controllers\Controller;
+use App\Models\Team;
+use Illuminate\Http\Request;
+
+class TeamsController extends Controller
+{
+    public function index()
+    {
+
+    }
+
+    public function create()
+    {
+    }
+
+    public function store(Request $request)
+    {
+    }
+
+    public function show(Team $team)
+    {
+    }
+
+    public function edit(Team $team)
+    {
+    }
+
+    public function update(Request $request, Team $team)
+    {
+    }
+
+    public function destroy(Team $team)
+    {
+    }
+}
