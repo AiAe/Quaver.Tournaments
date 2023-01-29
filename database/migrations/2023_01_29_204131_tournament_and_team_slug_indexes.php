3ebb1878a03012a205b9883737afaf7906e4cf46
AiAe <ayyayyedev@gmail.com>
2023-01-29 22:42:36 +0200

    Index team and tournament slug

---
+++ database/migrations/2023_01_29_204131_tournament_and_team_slug_indexes.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->index('slug');
+        });
+
+        Schema::table('tournaments', function (Blueprint $table) {
+            $table->index('slug');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('teams', function (Blueprint $table) {
+            $table->dropIndex('slug');
+        });
+
+        Schema::table('tournaments', function (Blueprint $table) {
+            $table->dropIndex('slug');
+        });
+    }
+};
