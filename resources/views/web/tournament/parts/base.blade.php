a5c646d0a5703541a7036bc0984b17cbd0690a77
AiAe <ayyayyedev@gmail.com>
2023-02-10 20:40:24 +0200

    Show alert when tournament is unlisted

--- resources/views/web/tournament/parts/base.blade.php
+++ resources/views/web/tournament/parts/base.blade.php
@@ -3,6 +3,14 @@
 @section('content')
     <div class="container">
         <div class="row">
+            @if(isset($tournament) && $tournament->status == \App\Enums\TournamentStatus::Unlisted)
+                <div class="col-lg-12">
+                    <div class="alert alert-danger text-center">
+                        {{ __('Tournament is Unlisted!') }}
+                    </div>
+                </div>
+            @endif
+
             @stack('cover')
         </div>
 
