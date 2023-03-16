3660fa1c2dc08288e5b2e32bf95029562ff02bfc
AiAe <me@aiae.dev>
2023-03-16 19:32:38 +0200

    Wip stuff

--- resources/views/web/tournament/components/sidebar.blade.php
+++ resources/views/web/tournament/components/sidebar.blade.php
@@ -1,22 +1,21 @@
 <div class="list-group mb-2">
     <a href="{{ route('web.tournaments.show', $tournament) }}"
        class="list-group-item {{ routeIs('web.tournaments.show') }}">
-        <i class="bi bi-info-square-fill"></i>
         {{ __('Information') }}
     </a>
     <a href="{{ route('web.tournaments.rules.show', $tournament) }}"
        class="list-group-item {{ routeIs('web.tournaments.rules.show') }}">
-        <i class="bi bi-hammer"></i>
         {{ __('Rules') }}
     </a>
+    <a href="#" class="list-group-item">
+        {{ __('Staff') }}
+    </a>
 </div>
-
 
 @can('create', [\App\Models\Team::class, $tournament])
     <div class="list-group mb-2">
         <a class="list-group-item" href="#tournamentRegister" data-bs-toggle="modal"
            data-bs-target="#tournamentRegister">
-            <i class="bi bi-box-arrow-right"></i>
             {{ __('Register') }}
         </a>
     </div>
@@ -30,7 +29,6 @@
         <div class="list-group mb-2">
             <a class="list-group-item {{ routeIs('web.tournaments.teams.show') }}"
                href="{{ route('web.tournaments.teams.show', ['tournament' => $tournament, 'team' => $team]) }}">
-                <i class="bi bi-people"></i>
                 {{ __('My Team') }}
             </a>
         </div>
@@ -49,13 +47,11 @@
     @if($tournament->format == TournamentFormat::Team)
         <a href="{{ route('web.tournaments.teams.index', ['tournament' => $tournament]) }}"
            class="list-group-item {{ routeIs('web.tournaments.teams.index') }}">
-            <i class="bi bi-controller"></i>
             {{ __('Teams') }}
         </a>
     @else
         <a href="{{ route('web.tournaments.teams.index', ['tournament' => $tournament]) }}"
            class="list-group-item {{ routeIs('web.tournaments.teams.index') }}">
-            <i class="bi bi-controller"></i>
             {{ __('Players') }}
         </a>
     @endif
@@ -63,41 +59,32 @@
 
 <div class="list-group mb-2">
     <a href="#" class="list-group-item">
-        <i class="bi bi-music-note-beamed"></i>
+        {{ __('Stages') }}
+    </a>
+    <a href="#" class="list-group-item">
         {{ __('Mappool') }}
     </a>
-    <a href="#" class="list-group-item">
-        <i class="bi bi-journal"></i>
-        {{ __('Bracket') }}
-    </a>
-    <a href="#" class="list-group-item">
-        <i class="bi bi-calendar"></i>
-        {{ __('Schedules') }}
-    </a>
+{{--    <a href="#" class="list-group-item">--}}
+{{--        <i class="bi bi-journal"></i>--}}
+{{--        {{ __('Bracket') }}--}}
+{{--    </a>--}}
+{{--    <a href="#" class="list-group-item">--}}
+{{--        {{ __('Schedules') }}--}}
+{{--    </a>--}}
 </div>
 
 <div class="list-group mb-2">
+{{--    <a href="#" class="list-group-item">--}}
+{{--        {{ __('Suggest Maps') }}--}}
+{{--    </a>--}}
     <a href="#" class="list-group-item">
-        <i class="bi bi-globe"></i>
-        {{ __('Suggest Maps') }}
-    </a>
-    <a href="#" class="list-group-item">
-        <i class="bi bi-shield-fill"></i>
         {{ __('Apply for Staff') }}
-    </a>
-</div>
-
-<div class="list-group mb-2">
-    <a href="#" class="list-group-item">
-        <i class="bi bi-people-fill"></i>
-        {{ __('Staff') }}
     </a>
 </div>
 
 @can('update', $tournament)
     <div class="list-group">
         <a href="#" class="list-group-item">
-            <i class="bi bi-wrench"></i>
             {{ __('Settings') }}
         </a>
     </div>
