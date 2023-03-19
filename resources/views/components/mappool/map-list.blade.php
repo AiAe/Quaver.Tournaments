5ebd4d81d0b7d51a5202469d0417758f39a7a631
AiAe <me@aiae.dev>
2023-03-19 23:07:28 +0200

    Mappools design WIP

--- resources/views/components/mappool/map-list.blade.php
+++ resources/views/components/mappool/map-list.blade.php
@@ -1,12 +1,45 @@
-<div>
-    <ul>
-        {{-- TODO: Implement map list design --}}
+<div class="collapse" id="mapsCollapse{{ $loop->index }}">
+    <div class="row mt-2">
         @forelse($maps as $map)
-            <li>
-                {{$map->map}} {{$map->mods}}
-            </li>
+            @php($m = $map->map)
+            <div class="col-lg-12 mb-2">
+                <div class="map position-relative">
+                    <div class="mapset-cover d-flex justify-content-between align-items-center"
+                         style="background: linear-gradient(90deg, rgba(23, 23, 23, 0.93) 52.58%, rgba(36, 36, 36, 0.4) 100%),
+                         url('https://cdn.quavergame.com/mapsets/{{ $m->quaver_mapset_id }}.jpg');">
+                        <div>
+                            <div>{{ $m->artist }} - {{ $m->title }}</div>
+                            <div>{{ $m->difficulty_name }} - {{ $m->difficulty_rating }}</div>
+                        </div>
+                        <div>
+                            {{ $map->mods }}
+                        </div>
+                    </div>
+                    <div class="row map-info-2">
+                        <div class="col-lg-8">
+                            {{ __('Category') }}: <span>{{ $map->category }} - {{ $map->sub_category }}</span>
+                        </div>
+                        <div class="col-lg-4">
+                            <div class="d-flex justify-content-between">
+                                <div>
+                                    <i class="bi bi-plus-slash-minus"></i> <span>{{ $map->offset }}</span>
+                                </div>
+                                <div>
+                                    <i class="bi bi-clock-fill"></i> <span>{{ $m->length }}</span>
+                                </div>
+                                <div>
+                                    <i class="bi bi-speedometer2"></i> <span>{{ $m->bpm }} BPM</span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <a href="{{ $m->quaverUrl() }}" class="stretched-link" target="_blank"></a>
+                </div>
+            </div>
         @empty
-            <li>No maps...</li>
+            <div class="col-lg-12">
+                {{ __('No maps...') }}
+            </div>
         @endforelse
-    </ul>
+    </div>
 </div>
