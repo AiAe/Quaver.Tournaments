6e0de54f84d1d0075e3eadf9e87d53095f7cc254
AiAe <me@aiae.dev>
2023-03-26 14:37:35 +0300

    Temporary fix

--- resources/views/livewire/tournaments/register.blade.php
+++ resources/views/livewire/tournaments/register.blade.php
@@ -8,7 +8,7 @@
                     <h5 class="modal-title" id="tournamentRegisterLabel">{{ __('Register') }}</h5>
                     <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                 </div>
-                @if($loggedUser->has_discord())
+                @if(Auth::user()->has_discord())
                     <form wire:submit.prevent="create">
                         @if($tournament->format == \App\Enums\TournamentFormat::Team)
                             <div class="modal-body">
