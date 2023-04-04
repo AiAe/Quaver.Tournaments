f99cc3c4c56ff9738a6e55d41c4899a4e70e7845
AiAe <me@aiae.dev>
2023-04-04 12:00:06 +0300

    Add more staff roles

--- app/Enums/StaffRole.php
+++ app/Enums/StaffRole.php
@@ -9,17 +9,23 @@ enum StaffRole: int
     use EnumToArray;
 
     case Organizer = 0;
-    case Mappooler = 1;
-    case Referee = 2;
-    case Streamer = 3;
-    case Commentator = 4;
+    case HeadMappooler = 1;
+    case Mappooler = 2;
+    case HeadReferee = 3;
+    case Referee = 4;
+    case HeadStreamer = 5;
+    case Streamer = 6;
+    case Commentator = 7;
 
     public function name(): string
     {
         return match ($this) {
             self::Organizer => 'Organizer',
+            self::HeadMappooler => 'Head Mappooler',
             self::Mappooler => 'Mappooler',
+            self::HeadReferee => 'Head Referee',
             self::Referee => 'Referee',
+            self::HeadStreamer => 'Head Streamer',
             self::Streamer => 'Streamer',
             self::Commentator => 'Commentator',
         };
