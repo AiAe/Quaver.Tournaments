24ac2a3053669e2f3d14e503c8684573166649b8
IceDynamix <me@icedynamix.moe>
2023-04-04 19:10:01 +0200

    Add match generator from match-ups

--- app/Helpers/MatchGenerator.php
+++ app/Helpers/MatchGenerator.php
@@ -3,6 +3,7 @@
 namespace App\Helpers;
 
 use App\Enums\MatchFormat;
+use App\Models\Team;
 use App\Models\TournamentStageRound;
 use Carbon\Carbon;
 
@@ -40,5 +41,54 @@ class MatchGenerator
                 'match_format' => MatchFormat::FreeForAll,
             ]);
         }
+    }
+
+    /**
+     * Creates matches from a nx2 array (array<[Team, Team]>) of teams. Intended to be used on group stage (swiss) stages.
+     * Considers team's set timezones to find an optimal time to play.
+     *
+     * @param TournamentStageRound $round
+     * @param array $matchUps
+     * @return void
+     */
+    public static function generateMatchesFromMatchUps(TournamentStageRound $round, array $matchUps)
+    {
+        $days = 3;
+        $maxMatchesPerDay = count($matchUps) / $days + 1;
+
+        $i = 1;
+        $matchesThisDay = 0;
+        $currentDay = $round->starts_at->endOfWeek()->copy();
+        foreach ($matchUps as [$team1, $team2]) {
+            if ($matchesThisDay > $maxMatchesPerDay) {
+                $currentDay->addDays(-1);
+                $matchesThisDay = 0;
+            }
+
+            $hour = self::findOptimalHourFromMatchUp($team1, $team2);
+            $timestamp = $currentDay->copy()->addHours($hour);
+
+            // TODO array insert
+            $round->matches()->create([
+                'label' => $i, // TODO: figure out labels
+                'team1_id' => $team1->id,
+                'team2_id' => $team2->id,
+                'timestamp' => $timestamp
+            ]);
+
+            $matchesThisDay++;
+        }
+    }
+
+    private static int $optimalHour = 19;
+
+    private static function findOptimalHourFromMatchUp(Team $team1, Team $team2): int
+    {
+        $time1 = $team1->timezone_offset;
+        $time2 = $team2->timezone_offset;
+
+        $difference = (($time1 - $time2 + 12) % 24) - 12;
+        $middleTimezone = (($time1 - $difference / 2 + 12) % 24) - 12;
+        return ((MatchGenerator::$optimalHour - $middleTimezone) % 24) / 24;
     }
 }
