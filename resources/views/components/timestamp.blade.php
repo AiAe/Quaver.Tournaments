6c76cc78b47113aea121450741cd6f32d5c9d070
AiAe <me@aiae.dev>
2023-04-15 12:53:39 +0300

    Add tooltip

--- resources/views/components/timestamp.blade.php
+++ resources/views/components/timestamp.blade.php
@@ -2,11 +2,9 @@
     'timestamp'
 ])
 
-{{-- TODO: tooltip --}}
-
-<span {{ $attributes }} class="badge bg-light text-dark">
+<span {{ $attributes }} class="badge bg-light text-dark" data-bs-toggle="tooltip" data-bs-placement="top" title="{{ $timestamp }} UTC">
     @if($loggedUser && $loggedUser->timezone_offset != null)
-        {{ $loggedUser->convertTime($timestamp) }} (local)
+        {{ $loggedUser->convertTime($timestamp) }} (UTC {{ $loggedUser->timezone_offset }})
     @else
         {{ $timestamp }}
     @endif
