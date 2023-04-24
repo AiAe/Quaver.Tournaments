764b6129b55ede564196225da5c85f8934941f3d
AiAe <me@aiae.dev>
2023-04-24 19:52:04 +0300

    Matches design

--- resources/views/components/timestamp.blade.php
+++ resources/views/components/timestamp.blade.php
@@ -1,9 +1,13 @@
 @props([
-    'timestamp'
+    'timestamp',
+    'has_title' => true
 ])
 
-<div class="small">{{ __('In your local time: ') }}</div>
-<span {{ $attributes }} class="badge bg-light text-dark" data-bs-toggle="tooltip" data-bs-placement="top" title="{{ $timestamp }} UTC">
+@if($has_title)
+    <div class="small">{{ __('In your local time: ') }}</div>
+@endif
+<span {{ $attributes }} class="badge bg-light text-dark" data-bs-toggle="tooltip" data-bs-placement="top"
+      title="{{ $timestamp }} UTC">
     @if($loggedUser && $loggedUser->timezone_offset != null)
         {{ $loggedUser->convertTime($timestamp) }} (UTC{{ format_timezone_offset($loggedUser->timezone_offset) }})
     @else
